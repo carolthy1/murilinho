ce2b4bc0f5e3783d43b5d605b6b4a095270bb782
carolthy1 <142441042+carolthy1@users.noreply.github.com>
2023-11-10 16:31:50 -0300

    1.0

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./styles.css", "./app.js"],
+  theme: {
+    colors: {
+      'green': "#bbf7d0",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
